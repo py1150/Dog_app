1db0aa3c1533b1215ebf02f5312b3c58f7165140
py1150 <bernd_galler@hotmail.com>
2019-04-19 22:41:39 +0200

    upload

--- classify_dogs.py
+++ classify_dogs.py
@@ -1,8 +1,73 @@
+import pdb
+
+
 # import necessary libraries
 #from keras.callbacks import ModelCheckpoint
+import numpy as np
 
 
-# load model
+# load face detector
+#-------------------
+import cv2
+
+
+# extract pre-trained face detector
+face_cascade = cv2.CascadeClassifier('saved_models/haarcascade_frontalface_alt.xml')
+
+def face_detector(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray)
+    return len(faces) > 0
+
+
+# load dog detector
+#-------------------
+
+import csv
+with open('saved_models/dog_names.csv') as f:
+    reader = csv.reader(f)
+    dog_names=[y for x in reader for y in x]
+
+
+
+from keras.preprocessing import image
+from tqdm import tqdm
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size=(224, 224))
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return np.expand_dims(x, axis=0)
+
+def paths_to_tensor(img_paths):
+    list_of_tensors = [path_to_tensor(img_path) for img_path in tqdm(img_paths)]
+    return np.vstack(list_of_tensors)
+
+
+from keras.applications.resnet50 import ResNet50
+
+# define ResNet50 model
+ResNet50_model = ResNet50(weights='imagenet')
+
+
+from keras.applications.resnet50 import preprocess_input, decode_predictions
+
+def ResNet50_predict_labels(img_path):
+    # returns prediction vector for image located at img_path
+    img = preprocess_input(path_to_tensor(img_path))
+    return np.argmax(ResNet50_model.predict(img))
+
+def dog_detector(img_path):
+    prediction = ResNet50_predict_labels(img_path)
+    return ((prediction <= 268) & (prediction >= 151))
+
+
+
+# load image classifier model
+#-----------------------------
 # reference to file path
 #VGG19_model.load_weights('saved_models/weights.best.VGG19.hdf5')
 print('Loading model \n')
@@ -30,7 +95,7 @@ def classify_dog_breed(img_path):
 
     # show image function
     def show_image(img_path):
-        import cv2
+        #import cv2
         import matplotlib.pyplot as plt
         #%matplotlib inline only valid for Jupyter notebook
 
@@ -75,7 +140,7 @@ def classify_dog_breed(img_path):
         return(type_i_dict)
 
     # Show image
-    show_image(img_path)
+    #show_image(img_path)
 
     # classify the image type: dog / human
     image_type=classify_type(img_path)
@@ -85,11 +150,13 @@ def classify_dog_breed(img_path):
         print('The image shows a dog.\n')
         prediction=VGG19_predict_breed(img_path)
         print('Predicted breed: %s' %prediction[15:len(prediction)])
+        return(prediction[15:len(prediction)])
 
     elif image_type['human']==True:
         print('The image shows a human.')
         prediction=VGG19_predict_breed(img_path)
         print('Resembled breed: %s' %prediction[15:len(prediction)])
+        return(prediction[15:len(prediction)])
         # show example image
         search=prediction[15:len(prediction)]
         test_list=[name for name in train_files if search in name]
--- run.py
+++ run.py
@@ -1,29 +1,89 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+from werkzeug.utils import secure_filename
 
-from flask import Flask, render_template, request
-from import classify_dogs import *
+print('start import classify_dogs')
+from classify_dogs import *
+
+
+# file upload
+#UPLOAD_FOLDER = 'static/images'
+#ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
 
 # Flask instance
 app = Flask(__name__)
 
+# define upload set
+photos=UploadSet('photos',IMAGES)
+# set destination folder for upload
+app.config['UPLOADED_PHOTOS_DEST']='static/images'
+# load configuration for upload sets
+configure_uploads(app,photos)
+
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 # define variables here
 testvar='teststr'
+
+# test prediction
+#test_image='/home/bernd/Pictures/Dachshund.jpg'
+test_image='/home/bernd/Documents/Python/Dog_app/static/images/Dachshund.jpg'
+test_pred=classify_dog_breed(test_image)
+print(test_pred)
+
+#def allowed_file(filename):
+#	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 # home
 @app.route('/', methods=['GET','POST'])
 def index():
-    if request.method=='POST':
-        img_path=request.form['path']
-        #return redirect(url_for('result'))
-        return redirect('result.html')
-    else:
-        return render_template('home.html')
+    #if request.method=='POST' and 'photo' in request.files:
+     #   filename=photos.save(request.files['photo'])
+      #  return filename
+        #flash('File(s) successfully uploaded')
+    return render_template('home.html')
+
+
+        # check if the post request has the file part
+        #if 'file' not in request.files:
+        #    flash('No file part')
+        #    return redirect(request.url)
+        #file = request.files['file']
+        #if file.filename == '':
+        #    flash('No file selected for uploading')
+        #    return redirect(request.url)
+        #if file and allowed_file(file.filename):
+        #    filename = secure_filename(file.filename)
+        #    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #    flash('File(s) successfully uploaded')
+            #return redirect('/result')
+        #    return render_template('result.html')
+    #else:
+        #return render_template('home.html')
+
 
 # result page
 @app.route('/result', methods=['GET','POST'])
 def result():
+    if request.method=='POST' and 'photo' in request.files:
+       filename=photos.save(request.files['photo'])
+       pred=classify_dog_breed('/home/bernd/Documents/Python/Dog_app/static/images/'+filename)
+       fullname='/home/bernd/Documents/Python/Dog_app/static/images/'+filename
+       return render_template('result.html', userval=filename, fullname=fullname)
 
-    #classify_dogs.classify_dog_breed(img_path)
-    return render_template('result.html', model=testvar)
+    #if request.method=='POST' and 'photo' in request.files:
+    #    filename = photos.save(request.files['photo'])
+    #    return render_template('result.html', userval=filename)
+        #    #classify_dog_breed(img_path)
+
+    #    img_path=request.form['path']
+    #else:
+    return render_template('home.html')
 
 if __name__ == '__main__':
+    app.secret_key = '123'
     app.run(debug=True)
